9b75ba27497900d6d8e8c61a75076e6b87704918
AntonKravchenko <kravchenko_ae@mail.ru>
2021-04-15 10:52:22 +0300

    HomeWork4

--- java-spring-mvc-lesson-three/src/main/java/com/example/controllers/TaskController.java
+++ java-spring-mvc-lesson-three/src/main/java/com/example/controllers/TaskController.java
@@ -5,10 +5,7 @@ import com.example.model.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -46,10 +43,9 @@ public class TaskController {
         return "task_views/product_form";
     }
 
-    @GetMapping("/delete{id}")
-    public String removeProduct(@PathVariable(value = "id") Long id, Model model) {
+    @PostMapping("/delete/{id}")
+    public String removeProduct(@PathVariable(value = "id") Long id) {
         productRepository.remove(id);
-        model.addAttribute("products", productRepository.findAll());
-        return "task_views/index";
+        return "redirect:/product";
     }
 }
