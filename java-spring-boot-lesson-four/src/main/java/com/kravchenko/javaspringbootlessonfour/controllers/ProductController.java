57602be11f529d8475944be2e1061fd548e74d38
AntonKravchenko <kravchenko_ae@mail.ru>
2021-04-27 11:50:55 +0300

    Homework7

--- java-spring-boot-lesson-four/src/main/java/com/kravchenko/javaspringbootlessonfour/controllers/ProductController.java
+++ java-spring-boot-lesson-four/src/main/java/com/kravchenko/javaspringbootlessonfour/controllers/ProductController.java
@@ -3,13 +3,19 @@ package com.kravchenko.javaspringbootlessonfour.controllers;
 import com.kravchenko.javaspringbootlessonfour.entities.Product;
 import com.kravchenko.javaspringbootlessonfour.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/product")
@@ -21,11 +27,18 @@ public class ProductController {
     @GetMapping
     public String indexPage(Model model, @RequestParam(name = "titleFilter", required = false) String titleFilter,
                             @RequestParam(name = "minFilter", required = false) BigDecimal minFilter,
-                            @RequestParam(name = "maxFilter", required = false) BigDecimal maxFilter) {
-        int currentPage = 1;
-        int maxProductsPerPage = 5;
+                            @RequestParam(name = "maxFilter", required = false) BigDecimal maxFilter,
+                            @RequestParam(name = "page") Optional<Integer> page,
+                            @RequestParam(name = "size") Optional<Integer> size) {
+        int currentPage = page.orElse(0);
+        model.addAttribute("currentPage", currentPage);
+        int pageSize = size.orElse(5);
+        model.addAttribute("pageSize", pageSize);
+
         List<Product> products = productService.getAllProduct();
         List<Product> productsByTitle;
+
+        //выбор продукта поиском по названию
         if (titleFilter == null || titleFilter.isBlank()) {
             productsByTitle = products;
         } else {
@@ -45,14 +58,37 @@ public class ProductController {
         if (maxFilter == null) {
             maxFilter = maxPrice;
         }
+        //выбор продукта по критерию минимума и максимума стоимости
         List<Product> productsByMinMax = productService.getByMinMaxCriteria(minFilter, maxFilter);
 
+        //поиск пересечения списков выбранных продуктов
         List<Product> selectedProducts = productsByTitle.stream()
                 .distinct()
                 .filter(productsByMinMax::contains)
                 .collect(Collectors.toList());
 
-        model.addAttribute("products", selectedProducts);
+        //попытка сделать паджинацию
+        int startProduct = currentPage * pageSize;
+        List<Product> productList;
+
+        if (selectedProducts.size() < startProduct) {
+            productList = Collections.emptyList();
+        } else {
+            productList = selectedProducts
+                    .subList(startProduct, Math.min(startProduct + pageSize, selectedProducts.size()));
+        }
+
+        int totalPages = selectedProducts.size() / pageSize + 1;
+        model.addAttribute("totalPages", totalPages);
+
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
+        model.addAttribute("products", productList);
         return "product_views/index";
     }
 
